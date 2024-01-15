48ee5d1106ce9cc1f61008d5c7567ed1270e8e97
Carlos Rodríguez <artesanomultimedia@gmail.com>
2024-01-15 03:05:10 +0100

    Corrijo errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
