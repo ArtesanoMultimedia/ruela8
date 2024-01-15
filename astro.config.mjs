e2ea6b13bffcfb38e4442e5ee64acb0506f83779
Carlos Rodríguez <artesanomultimedia@gmail.com>
2024-01-15 13:32:26 +0100

    elimino dependencias innecesarias y varias correcciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,17 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import react from "@astrojs/react";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      UnoCSS(),
     tailwind(),
     icon(),
-    react({
-      experimentalReactChildren: true,
-    })
   ],
   adapter: node({
     mode: "standalone"
   }),
-  vite: {
-    ssr: {
-      noExternal: ["react-icons"]
-    }
-  },
+  vite: {},
   site: "https://ruela8.netlify.app/"
 });
